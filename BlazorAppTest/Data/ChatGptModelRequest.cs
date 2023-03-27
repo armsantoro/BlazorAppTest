4bdc881f66bc0036d6f535eb573674a598a8331c
armsantoro <santoroarmando.as@gmail.com>
2023-03-27 16:16:44 +0200

    Aggiungere i file di progetto.

---
+++ BlazorAppTest/Data/ChatGptModelRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorAppTest.Data
+{
+    public class ChatGptModelRequest
+    {
+        public string model { get; set; }
+        public List<Message> messages { get; set; }
+    }
+
+    public class Message
+    {
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+}
---
+++ BlazorAppTest/Data/ChatGptService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace BlazorAppTest.Data
+{
+    public class ChatGptService
+    {
+        private readonly HttpClient _httpClient;
+        public ChatGptService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<string> GetResponseChatGpt(string prompt)
+        {
+            string apiUser = "user";
+            string apiKey = "INSERISCI LA CHIAVE";
+            string apiUrl = "https://api.openai.com/v1/chat/completions";
+
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+
+            ChatGptModelRequest chatGptModelRequest = new ChatGptModelRequest();
+            List<Message> messages = new List<Message>();
+            Message message = new Message();
+            message.role = apiUser;
+            message.content = prompt;
+            messages.Add(message);
+            
+            chatGptModelRequest.model = "gpt-3.5-turbo";
+            chatGptModelRequest.messages = messages;
+
+            var response = await _httpClient.PostAsJsonAsync(apiUrl, chatGptModelRequest);
+            var json = await response.Content.ReadAsStringAsync();
+
+            // Deserializza la stringa JSON in un oggetto ChatGptModelResponse
+            var chatGptModelResponse = JsonSerializer.Deserialize<ChatGptModelResponse>(json);
+
+            // Ottiene il contenuto del primo messaggio nella lista di messaggi
+            var content = chatGptModelResponse.choices[0].message.content;
+
+            return content;
+        }
+    }
+}
